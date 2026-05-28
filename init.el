73c7b45542d35d2f15073ef4feb87f96775dd269
Jukka Villstedt <jukka.villstedt@iki.fi>
2026-05-28 17:21:06 +0300

    accumulate returned chunks

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t; -*-
 (setq cider-extensions-source-file-path (concat (file-name-directory load-file-name)
                                                 "src/cider_extensions/core.clj"))
 
@@ -14,26 +15,27 @@
 (defun cider-extensions-autocompletions ()
   "Offer autocompletions cursor using the context around the cursor and the program runtime state."
   (interactive)
-  (cider-interactive-eval (format "(cider-extensions.core/autocompletions (quote %s) (quote %s))"
-                                  (cider-list-at-point)
-                                  (ignore-errors (save-excursion
-                                                   (up-list 2 t t)
-                                                   (cider-sexp-at-point))))
-                          (nrepl-make-response-handler (current-buffer)
-                                                       (lambda (buffer value-string)
-                                                         (when (not (equal "nil" value-string))
-                                                           (let ((value (read value-string)))
-                                                             (if (listp value)
-                                                                 (with-current-buffer buffer
-                                                                   (insert (first (helm :sources
-                                                                                        (helm-build-sync-source ""
-                                                                                          :candidates value)
-                                                                                        :buffer "*helm cider map keys*"))))
-                                                               (cider-emit-into-popup-buffer (cider-popup-buffer cider-result-buffer nil 'clojure-mode 'ancillary)
-                                                                                             value)))))
-                                                       (lambda (_buffer _output))
-                                                       (lambda (_buffer err)
-                                                         (cider-emit-interactive-eval-err-output err))
-                                                       '())
-                          nil
-                          (cider--nrepl-print-request-plist fill-column)))
+  (let ((accumulated-value ""))
+    (cider-interactive-eval (format "(cider-extensions.core/autocompletions (quote %s) (quote %s))"
+                                    (cider-list-at-point)
+                                    (ignore-errors (save-excursion
+                                                     (up-list 2 t t)
+                                                     (cider-sexp-at-point))))
+                            (nrepl-make-response-handler (current-buffer)
+                                                         ;; value handler: accumulate chunks
+                                                         (lambda (buffer value-string)
+                                                           (setq accumulated-value (concat accumulated-value value-string)))
+                                                         (lambda (_buffer _output))
+                                                         (lambda (_buffer err)
+                                                           (cider-emit-interactive-eval-err-output err))
+                                                         ;; done handler: process the complete accumulated value
+                                                         (lambda (buffer)
+                                                           (when (not (equal "nil" accumulated-value))
+                                                             (let ((value (read accumulated-value)))
+                                                               (if (listp value)
+                                                                   (with-current-buffer buffer
+                                                                     (insert (ivy-read "Choose: " value)))
+                                                                 (cider-emit-into-popup-buffer (cider-popup-buffer cider-result-buffer nil 'clojure-mode 'ancillary)
+                                                                                               value))))))
+                            nil
+                            (cider--nrepl-print-request-plist fill-column))))
